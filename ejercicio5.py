8b8aee663ce92948bedae13ca6953c0d401ae30c
Diego Veloz <134535944+VelozDVD@users.noreply.github.com>
2023-12-29 10:02:02 -0500

    Add files via upload

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,8 +1,14 @@
 print("Oi mundo, agora eu vou a falar em portugues para aprender")
-palavra = input("Digitar seu nome: ")
+palavra = input("Digitar seu nome: ").capitalize()
 if palavra == "pene" and palavra == "Verga" and palavra == "Puta":
      print("Oye no seas majadero cara de verga")
-     else:
-        sobrenome = input("Oi " + palavra + ", agora voce pode digitar se sobrenome: ")    
+else:
+     sobrenome = input("Oi " + palavra + ", agora voce pode digitar se sobrenome: ").capitalize()    
 
 print("Oi " + palavra + " " + sobrenome + " ¿Todo bem?") 
+print("Você sabe falar em português?")
+Resposta = input("Si/No: ").capitalize()
+if(Resposta == "Si"):
+     print("Muito bem! Eu estou muito feliz por você!")
+else:
+     print("Você tem que estudar para poder falar em português")
